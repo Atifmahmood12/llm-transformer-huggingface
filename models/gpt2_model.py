f84e06001e97d27d964e644601c7aedc2da52436
Atifmahmood12 <mr.atif@hotmail.com>
2025-09-22 21:17:58 +0100

    add the missing dependency in requirements file

--- models/gpt2_model.py
+++ models/gpt2_model.py
@@ -4,7 +4,7 @@ class GPT2Model:
     def __init__(self, model_name="gpt2"):
         self.generator = pipeline("text-generation", model=model_name)
 
-    def generate(self, prompt, max_new_tokens=50, temperature=1.0, do_sample=True):
+    def generate(self, prompt, max_new_tokens=50, temperature=1.0, do_sample=False):
         result = self.generator(
             prompt,
             max_new_tokens=max_new_tokens,
